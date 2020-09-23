1b34be87178eec4ea93de7258fdb34ce0f0e636c
Gobind Puniani <gobindpuniani@gmail.com>
2020-09-22 20:52:16 -0700

    Added list items

--- chat_bot.py
+++ chat_bot.py
@@ -9,21 +9,49 @@ from random import choice
 def get_bot_response(user_response):
 
     # Add several potential user responses in each list, where each list is a category of user response
-    user_response_affirmative = []
-    user_response_negatory = []
-    user_response_ambiguous = []
+    # No capital letters are included in these lists because the .lower() method is used in the if-statements
+    user_response_affirmative = [
+        "yes",
+        "definitely",
+        "sure",
+        "yeah",
+        "yes, please"
+        "ok",
+        "yup",
+        "couldn't hurt"
+    ]
+    user_response_negatory = [
+        "no",
+        "nope",
+        "definitely not",
+        "no thanks",
+        "i'm good",
+        "that's it"
+    ]
+    user_response_ambiguous = [
+        "perhaps",
+        "maybe",
+        "possibly"
+    ]
 
     #Add several bot responses in each list, where each list corresponds to a category of user response
-    bot_response_affirmative = []
+    bot_response_affirmative = [
+        "Attend office hours from 9-9:30am on Tuesdays and Thursdays in Jess' Zoom Room",
+        "Try getting help from other students and TAs at CoWork on Tuesdays and Thursdays from 1:30-2:30pm",
+        "Post questions on Piazza",
+        "Set up study/coding times with other students",
+        "Hit up any senior on Slack or Discord for help",
+    ]
     bot_response_negatory = []
     bot_response_ambiguous = []
 
     # Check which category the user_response falls into and return a random bot response from the corresponding category
-    if user_response in user_response_affirmative:
+    # The .lower() method converts the string into lower-case so that the user can enter a response with capitalization
+    if user_response.lower() in user_response_affirmative:
         return choice(bot_response_affirmative)
-    elif user_response in user_response_negatory:
+    elif user_response.lower() in user_response_negatory:
         return choice(bot_response_negatory)
-    elif user_response in user_response_ambiguous:
+    elif user_response.lower() in user_response_ambiguous:
         return choice(bot_response_ambiguous)
     else:
         return "Don't be afraid to take advantage of the support available to you!"
@@ -35,15 +63,16 @@ print("Simply follow the directions and type in a response when prompted.")
 print("When you're ready to terminate the program, please enter 'done' when prompted for a response.")
 
 while True:
-  user_response = input("Do you need help with CS 1.0?")
-  
-  # Quits program when user responds with 'done'
-  if user_response == "done":
-    break
+    # Prompt user for a response
+    user_response = input("Do you need more help with CS 1.0?")
 
-  
-  bot_response = get_bot_response(user_response)
-  print(bot_response)
+    # Quits program when user responds with 'done'
+    if user_response == "done":
+        break
+
+    # Call function for a response from the bot
+    bot_response = get_bot_response(user_response)
+    print(bot_response)
 
 
 
